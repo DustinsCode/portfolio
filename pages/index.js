b01db072a20b46ac80cea76ec8bf52c7d6d6ca21
Dustin Thurston <dthurston1996@gmail.com>
2022-01-22 21:50:40 -0500

    added some buttons that kinda do things

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,52 @@
+import { useEffect, useState } from 'react'
 import styles from '../styles/Home.module.scss'
 
+
 export default function Home() {
+
+  const [selection, setSelection] = useState('home')
+
+  function makeSelection(selection) {
+    setSelection(selection)
+  }
+
+  function getSelection() {
+    switch (selection) {
+      case 'home':
+        console.log('hit home')
+        return (
+          <div>
+            <h1>Let&apos;s make some cool shit.</h1>
+            <div className={styles.actionButtons}>
+              <button onClick={() => makeSelection('hellyeah')}>Hell Yeah!</button>
+              <button onClick={() => makeSelection('what')}>What kinda cool shit?</button>
+              <button onClick={() => makeSelection('who')}>Who even are you?</button>
+            </div>
+          </div>
+        )
+      case 'hellyeah':
+        console.log('hit hellyeah')
+        return (<div>success</div>)
+      case 'what':
+        console.log('hit what')
+        return (<div>what?</div>)
+      case 'who':
+        console.log('hit who')
+        return (<div>who?</div>)
+      default:
+        console.log('hit default')
+        makeSelection('home')
+    }
+  }
+
   return (
     <div className={styles.headerDiv}>
       <span className={styles.mainText}>
-        <h1>Let&apos;s make some cool shit.</h1>
+        {getSelection()}
       </span>
     </div>
   )
+
+
+
 }
